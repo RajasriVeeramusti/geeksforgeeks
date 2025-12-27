6bf37d71348b564778a91b202ce30a4002d46c6b
RajasriVeeramusti <165761211+RajasriVeeramusti@users.noreply.github.com>
2025-12-27 12:09:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthSmallest(int[][] mat, int k) {
+        // code here
+      PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
+      for(int[] row : mat){
+          for(int num:row){
+              pq.add(num);
+          }
+          while(pq.size()>k) pq.poll();
+      }
+      return pq.poll();
+    }
+}
